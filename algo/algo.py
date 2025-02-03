fa682d9ea75ee43b026059c87c51fbd204059d7b
Leonardo <leonardo.alonso@benandfrank.com>
2025-02-03 12:32:37 -0600

    algorithm base code

---
+++ algo/algo.py
@@ -0,0 +1,18 @@
+# 💡 Given a list of words, return the K most frequent ones in descending order of frequency.
+
+# For example, given the following list:
+# words = ["apple", "banana", "apple", "orange", "banana", "banana", "grape"]
+# k = 2
+
+# You should return ["banana", "apple"].
+
+
+
+def top_k_frequent(words, k):
+    pass
+
+# Example usage
+words = ["apple", "banana", "apple", "orange", "banana", "banana", "grape"]
+k = 2
+print(top_k_frequent(words, k))
+
